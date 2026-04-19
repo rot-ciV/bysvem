1922c50f10785b8c9ecabb1038d1baf03cc48244
rot-ciV <fagundes.nvictor@gmail.com>
2026-04-19 09:06:09 -0300

    Classe dev

--- Desenvolvedor.java
+++ Desenvolvedor.java
@@ -1,6 +1,44 @@
+import java.util.ArrayList;
+
 public class Desenvolvedor extends Conta{
-    public Operador(int id, String nome, int senha){
+
+    protected String desenvolvedora;
+    protected ArrayList<Jogo> jogosEmDesenvolvimento;
+    protected ArrayList<Jogo> jogosPublicados;
+    // (Victor) Diferenciei jogos que estão disponíveis para serem comprados e que ainda estão em desenvolvimento.
+
+    public Desenvolvedor(int id, String nome, int senha, String desenvolvedora){
+        
         super(id,nome,senha);
-        jogosadquiridos.add(JogoQueelecriar) //Se tentar compilar isso aqui vai dar certo nao em, isso aqui é um escopo prévio ainda
+        this.desenvolvedora = desenvolvedora;
+        this.jogosEmDesenvolvimento = new ArrayList<>();
+        this.jogosPublicados = new ArrayList<>();
+    }
+
+    // (Victor) Essa função apenas cria o jogo, mas ele ainda não está disponível na loja
+    public void criaJogo(int id, String nome, String genero, double preco){
+
+        Jogo novoJogo = new Jogo(id, nome, genero, this.desenvolvedora, preco);
+        jogosEmDesenvolvimento.add(novoJogo);
+    }
+
+    /* (Victor) Você dá o id do jogo que quer postar, caso o id exista na lista de jogos em desenv ele coloca o objeto
+    jogo na lista de jogos publicados. O boolean é apenas um retorno para saber se colocou ou não o jogo na lista publicados */ 
+    public boolean postaJogo(int id){
+
+        for(int i = 0; i < this.jogosEmDesenvolvimento.size(); i++){
+
+            Jogo buscaJogo = this.jogosEmDesenvolvimento.get(i);
+
+            if(buscaJogo.id == id){
+
+                this.jogosPublicados.add(buscaJogo);
+                this.jogosEmDesenvolvimento.remove(i);
+                
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Loja.java
+++ Loja.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class Loja extends Bysvem{
 
+    // A loja que centraliza todas as informações: Todos os jogos disponíveis, todas as contas existentes e todos os registros já feitos 
+    protected ArrayList<Jogo> jogos;
+    protected ArrayList<Conta> contas;
+    protected ArrayList<Registro> registros;
 }
