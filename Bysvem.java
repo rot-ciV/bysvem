532651d99dff098159a3a74e82f5afc833bc653c
Bernardoresende2006 <resendebr21@gmail.com>
2026-04-19 17:57:58 -0300

    Criação da main - Sujeita a mudanças

--- Bysvem.java
+++ Bysvem.java
@@ -18,4 +18,37 @@ public abstract class Bysvem{
 
     public void setId(int id){ this.id = id; }
     public int getId() { return this.id; }
+
+    private boolean verificador
+
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Olá, seja muito bem vindo ao Bysvem!\n");
+        while(true){
+            System.out.println("Por favor escolha uma das opções abaixo para continuar");
+            System.out.println("1)Entrar na loja        2)Sair");
+            int menu_option = nexInt();
+            if(menu_option == 1){
+                System.out.println("Para entrar na plataforma você precisa digitar o seu id:");
+                
+                int user_id = nexInt();
+                //verificar se o id existe
+                if(id existe){
+                    if(id é de dev){
+                        //nao sei como vai ser a análise disso, provavvelmente id específicos
+                    } else if(user_id == 17 || user_id == 24 || user_id == 67 || user_id == 69){
+                        //roda com a conta de um operador 
+                    } else{
+                        //roda com a conta de um usuário
+                    }
+                }
+            } else if(menu_option == 2){ 
+                break;
+                } else System.out.println("Infelizmente não temos essa opção.");
+            
+        }
+        leitor.close();
+        System.out.println("Obrigado por usar nossa loja, volte sempre!");
+
+    }
 }
--- Usuario.java
+++ Usuario.java
@@ -3,4 +3,6 @@ public class Usuario extends Conta{
     public Usuario(int id, String nome, int senha){
         super(id,nome,senha);
     }
+
+    
 }
